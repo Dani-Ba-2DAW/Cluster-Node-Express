4c56c8a98bff51552cc2d991f067012eaa60e5a2
DB <ddommor600@g.educaand.es>
2026-02-05 16:40:20 +0100

    Comprobación de funcionamiento sin clúster

--- provision.sh
+++ provision.sh
@@ -3,8 +3,11 @@
 apt-get update
 apt-get upgrade -y
 
-apt install -y npm
+apt install -y curl
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
+apt install -y nodejs
 
 npm init
 npm install express
 cp /vagrant/config/app.js .
+node app.js
